5a0bb1dc5a667c1b5215d111399ddb16738bd4ed
@Pramoda guruge <hasitha_20231715@fot.ruh.ac.lk>
2026-04-22 06:33:04 +0530

    My parts TG_1715 related to project

---
+++ NoticeDAO.java
@@ -0,0 +1,57 @@
+package com.example.javaminiproject.dao;
+
+import com.example.javaminiproject.db.DBConnection;
+import com.example.javaminiproject.model.Notice;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoticeDAO {
+
+    public List<Notice> getAll() throws SQLException {
+        List<Notice> list = new ArrayList<>();
+        String sql = "SELECT n.*, u.full_name FROM notices n " +
+                "LEFT JOIN users u ON n.created_by=u.user_id ORDER BY n.created_at DESC";
+        try (PreparedStatement ps = DBConnection.getInstance().getConnection().prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Notice n = new Notice(rs.getInt("notice_id"), rs.getString("title"),
+                        rs.getString("content"), rs.getInt("created_by"),
+                        rs.getTimestamp("created_at").toLocalDateTime());
+                n.setCreatedByName(rs.getString("full_name"));
+                list.add(n);
+            }
+        }
+        return list;
+    }
+
+    public void addNotice(Notice n) throws SQLException {
+        String sql = "INSERT INTO notices (title,content,created_by) VALUES (?,?,?)";
+        try (PreparedStatement ps = DBConnection.getInstance().getConnection().prepareStatement(sql)) {
+            ps.setString(1, n.getTitle());
+            ps.setString(2, n.getContent());
+            ps.setInt(3,    n.getCreatedBy());
+            ps.executeUpdate();
+        }
+    }
+
+    public void deleteNotice(int noticeId) throws SQLException {
+        String sql = "DELETE FROM notices WHERE notice_id=?";
+        try (PreparedStatement ps = DBConnection.getInstance().getConnection().prepareStatement(sql)) {
+            ps.setInt(1, noticeId);
+            ps.executeUpdate();
+        }
+    }
+    public boolean deleteNoticeByOwner(int noticeId, int ownerId) throws Exception {
+        String sql = "DELETE FROM notices WHERE notice_id = ? AND created_by = ?";
+
+        try (PreparedStatement ps = DBConnection.getInstance().getConnection().prepareStatement(sql)) {
+            ps.setInt(1, noticeId);
+            ps.setInt(2, ownerId);
+
+            int rows = ps.executeUpdate();
+            return rows > 0;
+        }
+    }
+}
